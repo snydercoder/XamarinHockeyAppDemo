491d2394020c707fbc5aa794c2de77f4f80ac549
Matthew Snyder <matt.snyder@calvarylogic.com>
2017-07-26 15:30:04 -0500

    Added missing files

---
+++ XamarinHockeyAppDemo.Ui/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Threading.Tasks;
+
+namespace XamarinHockeyAppDemo.Ui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+		public static Action ShowFeedbackScreenAction
+		{
+			get;
+			set;
+		}
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
